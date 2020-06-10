34efdfd13b428d0eef29756ccd38115f88fdbaaf
marek.kita <marek.kita@accenture.com>
2020-06-10 14:42:37 +0200

    4. Hiding Content with the [Hidden] Binding

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -10,11 +10,11 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
       <div>Date: {{event?.date}}</div>
       <div>Time: {{event?.time}}</div>
       <div>Price: \${{event?.price}}</div>
-      <div *ngIf="event?.location">
+      <div [hidden]="!event?.location"> <!--      <div *ngIf="event?.location">-->
         <span>Location: {{event?.location?.address}}</span>
         <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
       </div>
-      <div *ngIf="event?.onlineUrl">
+      <div [hidden]="!event?.onlineUrl"> <!--      <div *ngIf="event?.onlineUrl">-->
         Online URL: {{event.onlineUrl}}
       </div>
       <button class="btn btn-sm btn-primary" (click)="handleClickMe()">
