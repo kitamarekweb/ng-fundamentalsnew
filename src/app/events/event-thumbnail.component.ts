b0cf0adeb0c3fb4a4d37927a8ade59dd6a427ae9
marek.kita <marek.kita@accenture.com>
2020-06-10 15:30:21 +0200

    4. Styling Components with ngStyle

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,17 +8,35 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
+      <!--      ================================ -->
+      <!--      ========== Time START ========== -->
+      <!--      ================================ -->
+
 <!--======== Styling Components with ngClass START ============================================-->
       <!--      <div [class.green]="event?.time === '8:00 am'" //Class Binding-->
       <!--      <div [ngClass]="{pink: event?.time === '8:00 am', bold: event?.time === '8:00 am'}" //ngClass-->
       <!--      <div class="well" [ngClass]="getStartTimeClass()" //additional to well style it will also have ngClass or Class Binding-->
-      <div [ngClass]="getStartTimeClass()"
-           [ngSwitch]="event?.time">
+<!--      <div [ngClass]="getStartTimeClass()"-->
+<!--           [ngSwitch]="event?.time">-->
 <!--======== Styling Components with ngClass END ==============================================-->
+
+<!--======== Styling Components with ngStyle START ============================================-->
+        <!--      <div [style.color]="event?.time === '8:00 am' ? '#003300' : 'red'"-->
+        <!--           [ngSwitch]="event?.time">      -->
+        <!--      <div [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : 'red',-->
+        <!--                        'font-weight': event?.time === '8:00 am' ? 'bold' : 'normal'}"-->
+        <div [ngStyle]="getStartTimeStyle()"
+             [ngSwitch]="event?.time">
+<!--======== Styling Components with ngStyle END ==============================================-->
+
+        Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
-      </div>
+        </div>
+      <!--      ============================== -->
+      <!--      ========== Time END ========== -->
+      <!--      ============================== -->
       <div>Price: \${{event?.price}}</div>
       <div [hidden]="!event?.location"> <!--      <div *ngIf="event?.location">-->
         <span>Location: {{event?.location?.address}}</span>
@@ -85,6 +103,13 @@ export class EventThumbnailComponent {
       return ['purple', 'bold'];
     }
     return [];
+  }
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === '8:00 am') {
+      return {color: 'yellow', 'font-weight': 'bold'};
+    }
+    return {};
   }
 
 
