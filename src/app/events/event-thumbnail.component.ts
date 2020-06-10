7e87a6ad74ddcb975073f89bd61d6f7db23fe9f6
marek.kita <marek.kita@accenture.com>
2020-06-10 15:04:23 +0200

    4. Styling Components with ngClass – conditionally adding css classes to elements, Class Binding and ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,7 +8,13 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div [ngSwitch]="event?.time">
+<!--======== Styling Components with ngClass START ============================================-->
+      <!--      <div [class.green]="event?.time === '8:00 am'" //Class Binding-->
+      <!--      <div [ngClass]="{pink: event?.time === '8:00 am', bold: event?.time === '8:00 am'}" //ngClass-->
+      <!--      <div class="well" [ngClass]="getStartTimeClass()" //additional to well style it will also have ngClass or Class Binding-->
+      <div [ngClass]="getStartTimeClass()"
+           [ngSwitch]="event?.time">
+<!--======== Styling Components with ngClass END ==============================================-->
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
@@ -27,6 +33,22 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
     </div>
   `,
   styles: [`
+    .green {
+      color: #003300 !important;
+    }
+
+    .pink {
+      color: pink !important;
+    }
+
+    .purple {
+      color: purple !important;
+    }
+
+    .bold {
+      font-weight: bold;
+    }
+
     .thumbnail {
       min-height: 210px;
     }
@@ -46,6 +68,25 @@ export class EventThumbnailComponent {
   @Output() eventClick = new EventEmitter();
 
   someProperty: any = 'some property showing';
+
+  getStartTimeClass() {
+    // // returning object {}
+    // const isEarlyStart = this.event && this.event.time === '8:00 am'
+    // return {pink: isEarlyStart, bold: isEarlyStart};
+
+    // // returning string ''
+    // if (this.event && this.event.time === '8:00 am') {
+    //   return 'green bold';
+    // }
+    // return '';
+
+    // returning array []
+    if (this.event && this.event.time === '8:00 am') {
+      return ['purple', 'bold'];
+    }
+    return [];
+  }
+
 
   handleClickMe() {
     console.log('clicked');
