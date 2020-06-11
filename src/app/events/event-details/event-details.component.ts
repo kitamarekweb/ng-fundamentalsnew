3dc179c964db4adca04aea84b17851b90e673dae
marek.kita <marek.kita@accenture.com>
2020-06-12 00:57:44 +0200

    10. Creating a Filtering Display – buttons and filtering actions, [class.active] binding to set which button is active

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -25,6 +25,7 @@ import {IEvent, ISession} from '../shared';
 export class EventDetailsComponent implements OnInit {
   event: IEvent;
   addMode: boolean;
+  filterBy: string = 'all';
 
   constructor(private eventService: EventService, private route: ActivatedRoute) {
 
