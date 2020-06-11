333837d7f7a941e1ba8361d6d383bc70f709b0fe
marek.kita <marek.kita@accenture.com>
2020-06-11 16:35:58 +0200

    8. Passing Data out of a Child Component – Output property, styling anchor tag <a>, adding new component, using selector property of @Component, EventEmitter property, creating save method for new data object, PUSH event, UPDATE event method, SAVE event (saving to memory of the event), creating CANCEL method for the button, TOGGLING with views

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,9 +1,10 @@
 // ng-fundamentalsnew\src\app\events\event-details\create-session.component.ts START
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ISession, restrictedWords} from '../shared';
 
 @Component({
+  selector: 'create-session',
   templateUrl: './create-session.component.html',
   styles: [`
     em {
@@ -35,6 +36,9 @@ import {ISession, restrictedWords} from '../shared';
 })
 
 export class CreateSessionComponent implements OnInit {
+  @Output() saveNewSession = new EventEmitter();
+  @Output() cancelAddSession = new EventEmitter();
+
   newSessionForm: FormGroup;
   name: FormControl;
   presenter: FormControl;
@@ -75,7 +79,13 @@ export class CreateSessionComponent implements OnInit {
       voters: []
     };
     console.log(session);
+    this.saveNewSession.emit(session);
   }
+
+  cancel() {
+    this.cancelAddSession.emit()
+  }
+
 }
 
 // ng-fundamentalsnew\src\app\events\event-details\create-session.component.ts END
