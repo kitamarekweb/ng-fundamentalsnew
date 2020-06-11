b3aceeb06970cfefe74562fa0554651ee6a94a47
marek.kita <marek.kita@accenture.com>
2020-06-12 00:24:51 +0200

    9. Multiple Slot Content Projection – title and body section in Angular, optional select attribute with ng-content tag, class selectors

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -1,18 +1,25 @@
 // ng-fundamentals\src\app\common\collapsible-well.component.ts START
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'collapsible-well',
   template: `
     <div (click)="toggleContent()" class="well pointable">
-      <h4 class="well-title">{{title}}</h4>
-      <ng-content *ngIf="visible"></ng-content>
+<!--      <h4 class="well-title">{{title}}</h4>-->
+<!--      <ng-content *ngIf="visible"></ng-content>-->
+
+      <h4>
+        <!--    <ng-content select=".title"></ng-content>-->
+        <ng-content select="[well-title]"></ng-content>
+      </h4>
+      <!--    <ng-content *ngIf="visible" select=".body"></ng-content>-->
+      <ng-content *ngIf="visible" select="[well-body]"></ng-content>
     </div>
   `
 })
 
 export class CollapsibleWellComponent {
-  @Input() title: string;
+  // @Input() title: string;
   visible: boolean = true;
 
   toggleContent() {
