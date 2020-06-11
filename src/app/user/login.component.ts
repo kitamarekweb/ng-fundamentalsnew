b9bdf39f0ca45a990c2aa20993943085ac4b3535
marek.kita <marek.kita@accenture.com>
2020-06-11 14:42:54 +0200

    7. Validating Template-based Forms – loginForm attribute properties for validationValidating Template-based Forms – loginForm attribute properties for validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,15 +1,23 @@
 // ng-fundamentalsnew\src\app\user\login.component.ts START
 import {Component} from '@angular/core';
-import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em {
+      float: right;
+      color: #E05C65;
+      padding-left: 10px;
+    }
+  `]
 })
 
 export class LoginComponent {
   username;
   password;
+  mouseoverLogin;
 
   constructor(private authService: AuthService, private router: Router) {
   }
