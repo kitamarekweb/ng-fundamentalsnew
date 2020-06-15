bd2cb430a822ca46eab5444fcb1f30ae3a968cf3
marek.kita <marek.kita@accenture.com>
2020-06-15 12:29:54 +0200

    14. Using QueryString Parameters - searchSessions method

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,5 +1,5 @@
 // ng-fundamentals\src\app\events\shared\event.service.ts START
-import {EventEmitter, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {IEvent, ISession} from "./event.model";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
@@ -28,24 +28,9 @@ export class EventService {
       .pipe(catchError(this.handleError<IEvent>('saveEvent')));
   }
 
-  searchSessions(searchTerm: string) {
-    var term = searchTerm.toLocaleLowerCase();
-    var results: ISession[] = [];
-
-    EVENTS.forEach(event => {
-      var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1 );
-      matchingSessions = matchingSessions.map((session:any) => {
-        session.eventId = event.id;
-        return session;
-      });
-      results = results.concat(matchingSessions);
-    });
-
-    var emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-    return emitter;
+  searchSessions(searchTerm: string): Observable<ISession[]> {
+    return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions')))
   }
 
   private handleError<T> (operation = 'operation', result?: T) {
