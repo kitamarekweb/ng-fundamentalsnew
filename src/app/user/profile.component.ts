092f0d2a0b7cc86f1131e522125e1d75362fe744
marek.kita <marek.kita@accenture.com>
2020-06-15 14:07:08 +0200

    14. Saving User Data to the Server - updateCurrentUser method, saveProfile method

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit, Inject} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
-import {ToastrService, TOASTR_TOKEN} from '../common/toastr.service';
+import {ToastrService, TOASTR_TOKEN} from '../common';
 
 @Component({
   templateUrl: './profile.component.html',
@@ -60,10 +60,12 @@ export class ProfileComponent implements OnInit {
 
   saveProfile(formValues) {
     if (this.profileForm.valid) {
-      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-      console.log('first name:' + this.authService.currentUser.firstName + ', last name: ' + this.authService.currentUser.lastName);
-      // this.router.navigate(['events']); //before using TOASTR_TOKEN
-      this.toastr.success('Profile Saved');
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+        .subscribe(() => {
+          console.log('first name:' + this.authService.currentUser.firstName + ', last name: ' + this.authService.currentUser.lastName);
+          // this.router.navigate(['events']); //before using TOASTR_TOKEN
+          this.toastr.success('Profile Saved');
+        });
     }
   }
 
