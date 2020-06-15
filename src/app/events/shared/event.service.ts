c41010f1e315cd837ad9a01f265f9e462665270f
marek.kita <marek.kita@accenture.com>
2020-06-15 12:23:54 +0200

    14. Using POST and PUT - saveEvent method, save has post, update has put

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -2,7 +2,7 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {IEvent, ISession} from "./event.model";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 
 @Injectable()
@@ -19,18 +19,13 @@ export class EventService {
 
   getEvent(id: number): Observable<IEvent> {
     return this.http.get<IEvent>('/api/events/' + id)
-      .pipe(catchError(this.handleError<IEvent>('getEvents')))
+      .pipe(catchError(this.handleError<IEvent>('getEvent')))
   }
 
   saveEvent(event) {
-    event.id = 999;
-    event.session = [];
-    EVENTS.push(event);
-  }
-
-  updateEvent(event) {
-    let index = EVENTS.findIndex(x => x.id = event.id);
-    EVENTS[index] = event;
+    let options = { headers: new HttpHeaders({'Content-Type': 'application/json'})}
+    return this.http.post<IEvent>('/api/events', event, options)
+      .pipe(catchError(this.handleError<IEvent>('saveEvent')));
   }
 
   searchSessions(searchTerm: string) {
