273d1d867c26666b1138865348058401256fe07d
marek.kita <marek.kita@accenture.com>
2020-06-15 11:16:38 +0200

    14. Moving Data Storage to the Server - created HTTP call using RxJS and HttpClient, Subscribe to the Observable, optional parameter 'result?: T'

--- src/app/events/events-list-resolver.service.ts
+++ src/app/events/events-list-resolver.service.ts
@@ -1,8 +1,7 @@
 // ng-fundamentalsnew\src\app\events\events-list-resolver.service.ts START
 import {Injectable} from '@angular/core';
 import {Resolve} from '@angular/router';
-import {EventService} from './shared/event.service';
-import {map} from 'rxjs/operators';
+import {EventService} from './shared';
 
 @Injectable()
 export class EventsListResolver implements Resolve<any> {
@@ -11,8 +10,8 @@ export class EventsListResolver implements Resolve<any> {
   }
 
   resolve() {
-    console.log('async imitation');
-    return this.eventService.getEvents().pipe(map(events => events));
+    console.log('getting data from http request');
+    return this.eventService.getEvents();
   }
 }
 
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,17 +1,27 @@
 // ng-fundamentals\src\app\events\shared\event.service.ts START
 import {EventEmitter, Injectable} from '@angular/core';
-import {Subject, Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {IEvent, ISession} from "./event.model";
+import {HttpClient} from "@angular/common/http";
+import {catchError} from "rxjs/operators";
 
 @Injectable()
 export class EventService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
   getEvents(): Observable<IEvent[]> {
-    let subject = new Subject<IEvent[]>();
-    setTimeout(() => {
-      subject.next(EVENTS);
-      subject.complete();
-    }, 1500);
-    return subject;
+    return this.http.get<IEvent[]>('/api/events')
+      .pipe(catchError(this.handleError<IEvent[]>('getEvents', [])));
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(operation + ' : ' + error);
+      return of(result as T);
+    }
   }
 
   getEvent(id: number) {
